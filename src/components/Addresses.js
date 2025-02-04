a96f77e508dc25aea2b50423b1cd2725549d6617
saki-osive <saki.osive@gmail.com>
2025-02-03 22:39:37 -0500

    add light background

--- src/components/Addresses.js
+++ src/components/Addresses.js
@@ -1,54 +1,56 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { useNavigate } from 'react-router-dom';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import Title from "./Title";
-import {Link} from "react-router-dom";
 
 // Generate Order Data
 function createData(id, address) {
     return { id, address };
 }
 
-const rows = [
-    createData(
-        0,
-        '0xb794f5ea0ba39494ce839613fffba74279579268',
-    ),
-    createData(
-        1,
-        '0xbe0eb53f46cd790cd13851d5eff43d12404d33e8',
-    ),
-    createData(
-        2,
-        '0xda9dfa130df4de4673b89022ee50ff26f6ea73cf',
-    ),
+const addresses = [
+    createData(0, '0xb794f5ea0ba39494ce839613fffba74279579268'),
+    createData(1, '0xbe0eb53f46cd790cd13851d5eff43d12404d33e8'),
+    createData(2, '0xda9dfa130df4de4673b89022ee50ff26f6ea73cf'),
 ];
 
 export default function Addresses() {
+    const [selected, setSelected] = React.useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        const addr = event.target.value;
+        setSelected(addr);
+        if (addr) {
+            navigate(`/transfer/${addr}`);
+        }
+    };
+
     return (
-        <React.Fragment >
+        <React.Fragment>
             <Title>Blockchain Node Addresses</Title>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Select Address to Transfer</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell>
-                                <Link to={`/transfer/${row.address}`}>
-                                    {row.address}
-                                </Link>
-                            </TableCell>
-                        </TableRow>
+            <FormControl variant="outlined" fullWidth>
+                <InputLabel id="address-select-label">Select Address to Transfer</InputLabel>
+                <Select
+                    labelId="address-select-label"
+                    id="address-select"
+                    value={selected}
+                    onChange={handleChange}
+                    label="Select Address to Transfer"
+                >
+                    <MenuItem value="" disabled>
+                        <em>Select an Address</em>
+                    </MenuItem>
+                    {addresses.map((addr) => (
+                        <MenuItem key={addr.id} value={addr.address}>
+                            {addr.address}
+                        </MenuItem>
                     ))}
-                </TableBody>
-            </Table>
+                </Select>
+            </FormControl>
         </React.Fragment>
     );
 }
